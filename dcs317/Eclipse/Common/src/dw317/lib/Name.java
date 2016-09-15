787eab4439d51c6c58d461027916f514d8a6087f
alman <alman@localhost>
2016-09-15 19:20:43 -0400

    this is the Name class, let me know if you want to make some changes

---
+++ dcs317/Eclipse/Common/src/dw317/lib/Name.java
@@ -0,0 +1,98 @@
+package dw317.lib;
+import java.io.*;
+
+public class Name implements Serializable {
+
+
+	
+
+	private String firstName;
+	private String lastName;
+	private static final long serialVersionUID = 42031768871L;
+
+	public Name(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName(){
+		return firstName +" "+ lastName;
+	}
+
+	public String toString(){
+		return firstName +" "+ lastName;
+	}
+
+	/*
+	@Override
+	 */
+	public int hashCode() {
+		firstName = firstName.toUpperCase();
+		lastName = lastName.toUpperCase();
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	
+	/*
+	@Override
+	 */
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Name other = (Name) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+	private boolean isValide(){
+		
+		for(int i = 0; i < firstName.length(); i++){
+			if(!Character.isAlphabetic(firstName.charAt(i)) && firstName.charAt(i) != '\'' 
+					&& firstName.charAt(i) != '-' && firstName.charAt(i) != ' '){
+				throw new IllegalArgumentException();
+			}
+		}
+		for(int i = 0; i < lastName.length(); i++){
+			if(!Character.isAlphabetic(lastName.charAt(i)) && lastName.charAt(i) != '\'' 
+					&& lastName.charAt(i) != '-' && lastName.charAt(i) != ' '){
+				throw new IllegalArgumentException();
+			}
+		}
+		return true;
+	}
+}
